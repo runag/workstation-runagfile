1891f748a05258c7e896d5021fabc56e73d7bee9
Stanislav Senotrusov <stan@senotrusov.com>
2020-04-25 17:12:15 +0800

    source all files

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -15,20 +15,10 @@
 #  limitations under the License.
 
 deploy::data-pi() {
-  . data-pi/data-pi-lib.sh    || fail "Unable to load data-pi/data-pi-lib.sh"
-  . netdata/netdata-lib.sh    || fail "Unable to load netdata/netdata-lib.sh"
-  . ubuntu/ubuntu-lib.sh      || fail "Unable to load ubuntu/ubuntu-lib.sh"
-  . ubuntu/ubuntu-packages.sh || fail "Unable to load ubuntu/ubuntu-packages.sh"
-
   ubuntu::deploy-data-pi || fail
 }
 
 deploy::macos-workstation() {
-  . data-pi/data-pi-lib.sh  || fail "Unable to load data-pi/data-pi-lib.sh"
-  . macos/macos-lib.sh      || fail "Unable to load macos/macos-lib.sh"
-  . sublime/sublime-lib.sh  || fail "Unable to load sublime/sublime-lib.sh"
-  . vscode/vscode-lib.sh    || fail "Unable to load vscode/vscode-lib.sh"
-
   macos::deploy-workstation || fail
 }
 
@@ -37,13 +27,6 @@ deploy::macos-non-developer-workstation() {
 }
 
 deploy::ubuntu-workstation() {
-  . data-pi/data-pi-lib.sh    || fail "Unable to load data-pi/data-pi-lib.sh"
-  . sublime/sublime-lib.sh    || fail "Unable to load sublime/sublime-lib.sh"
-  . sway/sway-lib.sh          || fail "Unable to load ubuntu/sway.sh"
-  . ubuntu/ubuntu-lib.sh      || fail "Unable to load ubuntu/ubuntu-lib.sh"
-  . ubuntu/ubuntu-packages.sh || fail "Unable to load ubuntu/ubuntu-packages.sh"
-  . vscode/vscode-lib.sh      || fail "Unable to load vscode/vscode-lib.sh"
-
   ubuntu::deploy-workstation || fail
 }
 
@@ -52,10 +35,6 @@ deploy::ubuntu-sway-workstation() {
 }
 
 deploy::merge-workstation-configs() {
-  . sublime/sublime-lib.sh || fail "Unable to load sublime/sublime-lib.sh"
-  . sway/sway-lib.sh       || fail "Unable to load sway/sway-lib.sh"
-  . vscode/vscode-lib.sh   || fail "Unable to load vscode/vscode-lib.sh"
-
   deploy-lib::footnotes::init || fail
 
   vscode::merge-config || fail
