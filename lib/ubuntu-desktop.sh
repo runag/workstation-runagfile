fb8203edfa39f2774eadf2eefeadad584f5f4db2
Stanislav Senotrusov <stan@senotrusov.com>
2020-07-15 04:17:31 +0800

    minor ubuntu refactor

---
+++ lib/ubuntu-desktop.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+
+#  Copyright 2012-2019 Stanislav Senotrusov <stan@senotrusov.com>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+ubuntu::desktop::remove-user-dirs() {
+  local dirsFile="${HOME}/.config/user-dirs.dirs"
+
+  if [ -f "${dirsFile}" ]; then
+    local tmpFile; tmpFile="$(mktemp)" || fail
+
+    if [ -d "$HOME/Desktop" ]; then
+      echo 'XDG_DESKTOP_DIR="$HOME/Desktop"' >>"${tmpFile}" || fail
+    fi
+
+    if [ -d "$HOME/Downloads" ]; then
+      echo 'XDG_DOWNLOADS_DIR="$HOME/Downloads"' >>"${tmpFile}" || fail
+    fi
+
+    mv "${tmpFile}" "${dirsFile}" || fail
+
+    echo 'enabled=false' >"${HOME}/.config/user-dirs.conf" || fail
+
+    fs::remove-dir-if-empty "$HOME/Documents" || fail
+    fs::remove-dir-if-empty "$HOME/Music" || fail
+    fs::remove-dir-if-empty "$HOME/Pictures" || fail
+    fs::remove-dir-if-empty "$HOME/Public" || fail
+    fs::remove-dir-if-empty "$HOME/Templates" || fail
+    fs::remove-dir-if-empty "$HOME/Videos" || fail
+
+    if [ -f "$HOME/examples.desktop" ]; then
+      rm "$HOME/examples.desktop" || fail
+    fi
+
+    xdg-user-dirs-update || fail
+  fi
+}
+
+ubuntu::desktop::configure() {
+  # use dconf-editor to find key/value pairs
+  # Don't use dbus-launch here because it will introduce side-effect to
+  # git::ubuntu::add-credentials-to-keyring and ssh::ubuntu::add-key-password-to-keyring
+
+  if [ -n "${DBUS_SESSION_BUS_ADDRESS:-}" ]; then
+    # Terminal
+    if gsettings get org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled; then
+      gsettings set org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled false || fail
+    fi
+
+    if gsettings get org.gnome.Terminal.ProfilesList default; then
+      local terminalProfile; terminalProfile="$(gsettings get org.gnome.Terminal.ProfilesList default)" || fail "Unable to determine terminalProfile ($?)"
+      local profilePath="org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${terminalProfile:1:-1}/"
+
+      gsettings set "$profilePath" exit-action 'hold' || fail
+      gsettings set "$profilePath" login-shell true || fail
+    fi
+
+    # Nautilus
+    if gsettings list-keys org.gnome.nautilus; then
+      gsettings set org.gnome.nautilus.list-view default-zoom-level 'small' || fail
+      gsettings set org.gnome.nautilus.list-view use-tree-view true || fail
+      gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view' || fail
+      gsettings set org.gnome.nautilus.preferences show-delete-permanently true || fail
+      gsettings set org.gnome.nautilus.preferences show-hidden-files true || fail
+    fi
+
+    # Desktop 18.04
+    if gsettings list-keys org.gnome.nautilus.desktop; then
+      gsettings set org.gnome.nautilus.desktop trash-icon-visible false || fail
+      gsettings set org.gnome.nautilus.desktop volumes-visible false || fail
+    fi
+
+    # Desktop 19.10
+    if gsettings list-keys org.gnome.shell.extensions.desktop-icons; then
+      gsettings set org.gnome.shell.extensions.desktop-icons show-trash false || fail
+      gsettings set org.gnome.shell.extensions.desktop-icons show-home false || fail
+    fi
+
+    # Disable screen lock
+    if gsettings get org.gnome.desktop.session idle-delay; then
+      gsettings set org.gnome.desktop.session idle-delay 0 || fail
+    fi
+
+    # Auto set time zone
+    if gsettings get org.gnome.desktop.datetime automatic-timezone; then
+      gsettings set org.gnome.desktop.datetime automatic-timezone true || fail
+    fi
+
+    # Dash appearance
+    if gsettings get org.gnome.shell.extensions.dash-to-dock dash-max-icon-size; then
+      gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 38 || fail
+    fi
+
+    # Sound alerts
+    if gsettings get org.gnome.desktop.sound event-sounds; then
+      gsettings set org.gnome.desktop.sound event-sounds false || fail
+    fi
+
+    # 1600 DPI mouse
+    if gsettings get org.gnome.desktop.peripherals.mouse speed; then
+      gsettings set org.gnome.desktop.peripherals.mouse speed -0.75 || fail
+    fi
+
+    # Input sources
+    # ('xkb', 'ru+mac')
+    if gsettings get org.gnome.desktop.input-sources sources; then
+      gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'ru')]" || fail
+    fi
+
+    # Enable fractional scaling
+    if gsettings get org.gnome.mutter experimental-features; then
+      gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer', 'x11-randr-fractional-scaling']" || fail
+    fi
+  fi
+}
--- lib/ubuntu.sh
+++ lib/ubuntu.sh
@@ -17,8 +17,10 @@
 ubuntu::deploy-workstation() {
   ubuntu::install-packages || fail
   ubuntu::configure-workstation || fail
-  ubuntu::display-if-restart-required || fail
-  tools::display-elapsed-time || fail
+  if [ -t 1 ]; then
+    ubuntu::display-if-restart-required || fail
+    tools::display-elapsed-time || fail
+  fi
 }
 
 ubuntu::install-packages() {
@@ -27,10 +29,10 @@ ubuntu::install-packages() {
   apt::dist-upgrade || fail
 
   # basic tools, contains curl so it have to be first
-  ubuntu::install-packages::basic-tools || fail
+  ubuntu::packages::install-basic-tools || fail
 
   # devtools
-  ubuntu::install-packages::devtools || fail
+  ubuntu::packages::install-devtools || fail
 
   # git credential libsecret
   git::ubuntu::install-credential-libsecret || fail
@@ -92,54 +94,16 @@ ubuntu::install-packages() {
     sudo snap install discord || fail
     sudo snap install skype --classic || fail
     sudo snap install telegram-desktop || fail
-
-    if ! command -v libreoffice >/dev/null; then
-      sudo snap install libreoffice || fail
-    fi
   fi
 
   # Cleanup
   apt::autoremove || fail
-
-  # sudo gem install rake solargraph || fail "Unable to install gems"
-  # sudo gem update --system || fail "Unable to execute gem update --system"
-  # sudo gem update || fail "Unable to update gems"
-}
-
-ubuntu::install-packages::basic-tools() {
-  apt::install \
-    curl \
-    git \
-    jq \
-    mc ranger ncdu \
-    htop \
-    p7zip-full \
-    tmux \
-    sysbench \
-    direnv \
-    debian-goodies \
-      || fail
-}
-
-ubuntu::install-packages::devtools() {
-  apt::install \
-    build-essential autoconf bison libncurses-dev libffi-dev libgdbm-dev libreadline-dev libssl-dev zlib1g-dev libyaml-dev libxml2-dev libxslt-dev \
-    postgresql libpq-dev postgresql-contrib \
-    sqlite3 libsqlite3-dev \
-    redis-server \
-    memcached \
-    ruby-full \
-    python3-pip python3-psycopg2 \
-    ffmpeg imagemagick ghostscript libgs-dev \
-    graphviz \
-    shellcheck \
-    apache2-utils \
-    inotify-tools \
-    awscli \
-      || fail
 }
 
 ubuntu::configure-workstation() {
+  # increase inotify limits
+  ubuntu::set-inotify-max-user-watches || fail
+
   # shellrcd
   shellrcd::install || fail
   shellrcd::use-nano-editor || fail
@@ -150,6 +114,7 @@ ubuntu::configure-workstation() {
   ruby::configure-gemrc || fail
   shellrcd::rbenv || fail
   rbenv rehash || fail
+  sudo gem update || fail
 
   # nodejs
   shellrcd::nodenv || fail
@@ -162,11 +127,13 @@ ubuntu::configure-workstation() {
   # sublime text
   sublime::install-config || fail
 
-  # increase inotify limits
-  ubuntu::set-inotify-max-user-watches || fail
+  # enable syncthing
+  # if ubuntu::is-bare-metal; then
+  #   sudo systemctl enable --now "syncthing@${SUDO_USER}.service" || fail
+  # fi
 
   # configure desktop
-  ubuntu::configure-desktop || fail
+  ubuntu::desktop::configure || fail
 
   # IMWhell
   ubuntu::desktop::setup-imwhell || fail
@@ -181,7 +148,7 @@ ubuntu::configure-workstation() {
   ubuntu::desktop::moz-enable-wayland || fail
 
   # remove user dirs
-  ubuntu::remove-user-dirs || fail
+  ubuntu::desktop::remove-user-dirs || fail
 
   # hide folders
   ubuntu::desktop::hide-folder "Desktop" || fail
@@ -194,11 +161,6 @@ ubuntu::configure-workstation() {
     ubuntu::vmware::symlink-hgfs-mounts || fail
   fi
 
-  # enable syncthing
-  # if ubuntu::is-bare-metal; then
-  #   sudo systemctl enable --now "syncthing@${SUDO_USER}.service" || fail
-  # fi
-
   # SSH keys
   ssh::install-keys || fail
   ssh::ubuntu::add-key-password-to-keyring || fail
@@ -206,115 +168,4 @@ ubuntu::configure-workstation() {
   # git
   git::configure || fail
   git::ubuntu::add-credentials-to-keyring || fail
-}
-
-ubuntu::remove-user-dirs() {
-  local dirsFile="${HOME}/.config/user-dirs.dirs"
-
-  if [ -f "${dirsFile}" ]; then
-    local tmpFile; tmpFile="$(mktemp)" || fail
-
-    if [ -d "$HOME/Desktop" ]; then
-      echo 'XDG_DESKTOP_DIR="$HOME/Desktop"' >>"${tmpFile}" || fail
-    fi
-
-    if [ -d "$HOME/Downloads" ]; then
-      echo 'XDG_DOWNLOADS_DIR="$HOME/Downloads"' >>"${tmpFile}" || fail
-    fi
-
-    mv "${tmpFile}" "${dirsFile}" || fail
-
-    echo 'enabled=false' >"${HOME}/.config/user-dirs.conf" || fail
-
-    fs::remove-dir-if-empty "$HOME/Documents" || fail
-    fs::remove-dir-if-empty "$HOME/Music" || fail
-    fs::remove-dir-if-empty "$HOME/Pictures" || fail
-    fs::remove-dir-if-empty "$HOME/Public" || fail
-    fs::remove-dir-if-empty "$HOME/Templates" || fail
-    fs::remove-dir-if-empty "$HOME/Videos" || fail
-
-    if [ -f "$HOME/examples.desktop" ]; then
-      rm "$HOME/examples.desktop" || fail
-    fi
-
-    xdg-user-dirs-update || fail
-  fi
-}
-
-ubuntu::configure-desktop() {
-  # use dconf-editor to find key/value pairs
-  # Don't use dbus-launch here because it will introduce side-effect to
-  # git::ubuntu::add-credentials-to-keyring and ssh::ubuntu::add-key-password-to-keyring
-
-  if [ -n "${DBUS_SESSION_BUS_ADDRESS:-}" ]; then
-    # Terminal
-    if gsettings get org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled; then
-      gsettings set org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled false || fail
-    fi
-
-    if gsettings get org.gnome.Terminal.ProfilesList default; then
-      local terminalProfile; terminalProfile="$(gsettings get org.gnome.Terminal.ProfilesList default)" || fail "Unable to determine terminalProfile ($?)"
-      local profilePath="org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${terminalProfile:1:-1}/"
-
-      gsettings set "$profilePath" exit-action 'hold' || fail
-      gsettings set "$profilePath" login-shell true || fail
-    fi
-
-    # Nautilus
-    if gsettings list-keys org.gnome.nautilus; then
-      gsettings set org.gnome.nautilus.list-view default-zoom-level 'small' || fail
-      gsettings set org.gnome.nautilus.list-view use-tree-view true || fail
-      gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view' || fail
-      gsettings set org.gnome.nautilus.preferences show-delete-permanently true || fail
-      gsettings set org.gnome.nautilus.preferences show-hidden-files true || fail
-    fi
-
-    # Desktop 18.04
-    if gsettings list-keys org.gnome.nautilus.desktop; then
-      gsettings set org.gnome.nautilus.desktop trash-icon-visible false || fail
-      gsettings set org.gnome.nautilus.desktop volumes-visible false || fail
-    fi
-
-    # Desktop 19.10
-    if gsettings list-keys org.gnome.shell.extensions.desktop-icons; then
-      gsettings set org.gnome.shell.extensions.desktop-icons show-trash false || fail
-      gsettings set org.gnome.shell.extensions.desktop-icons show-home false || fail
-    fi
-
-    # Disable screen lock
-    if gsettings get org.gnome.desktop.session idle-delay; then
-      gsettings set org.gnome.desktop.session idle-delay 0 || fail
-    fi
-
-    # Auto set time zone
-    if gsettings get org.gnome.desktop.datetime automatic-timezone; then
-      gsettings set org.gnome.desktop.datetime automatic-timezone true || fail
-    fi
-
-    # Dash appearance
-    if gsettings get org.gnome.shell.extensions.dash-to-dock dash-max-icon-size; then
-      gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 38 || fail
-    fi
-
-    # Sound alerts
-    if gsettings get org.gnome.desktop.sound event-sounds; then
-      gsettings set org.gnome.desktop.sound event-sounds false || fail
-    fi
-
-    # 1600 DPI mouse
-    if gsettings get org.gnome.desktop.peripherals.mouse speed; then
-      gsettings set org.gnome.desktop.peripherals.mouse speed -0.75 || fail
-    fi
-
-    # Input sources
-    # ('xkb', 'ru+mac')
-    if gsettings get org.gnome.desktop.input-sources sources; then
-      gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'ru')]" || fail
-    fi
-
-    # Enable fractional scaling
-    if gsettings get org.gnome.mutter experimental-features; then
-      gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer', 'x11-randr-fractional-scaling']" || fail
-    fi
-  fi
 }
