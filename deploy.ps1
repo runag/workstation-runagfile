47e6c2e999953c2093d106dd2ceb4242faceeaac
Stanislav Senotrusov <stan@senotrusov.com>
2020-07-19 16:41:30 +0800

    better error handling

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,11 @@ if (-Not (Get-Command "choco" -ErrorAction SilentlyContinue)) {
   Set-ExecutionPolicy Bypass -Scope Process -Force
   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
   Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
   choco feature enable -n allowGlobalConfirmation
+  if ($LASTEXITCODE -ne 0) {
+    throw "Unable to set chocolatey feature"
+  }
 }
 
 Set-Service -Name ssh-agent -StartupType Automatic
@@ -12,14 +16,23 @@ Set-Service -Name ssh-agent -Status Running
 
 if (-Not (Get-Command "bw" -ErrorAction SilentlyContinue)) {
   choco install bitwarden-cli
+  if ($LASTEXITCODE -ne 0) {
+    throw "Unable to install bitwarden-cli"
+  }
 }
 
 if (-Not (Get-Command "jq" -ErrorAction SilentlyContinue)) {
   choco install jq
+  if ($LASTEXITCODE -ne 0) {
+    throw "Unable to install jq"
+  }
 }
 
 if (-Not (Get-Command "code" -ErrorAction SilentlyContinue)) {
   choco install vscode
+  if ($LASTEXITCODE -ne 0) {
+    throw "Unable to install vscode"
+  }
 }
 
 if (Test-Path "C:\Program Files\Git\bin\sh.exe") {
