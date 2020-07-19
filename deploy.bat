9ed71d9216b8cea2749fa2b7732a1c21ecc1cab7
Stanislav Senotrusov <stan@senotrusov.com>
2020-07-19 22:14:30 +0800

    fixes

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,5 @@
 @echo off
 
-cd %USERPROFILE% || echo "Unable to change directory" && exit /B
+curl -Ssf https://raw.githubusercontent.com/senotrusov/stan-computer-deploy/master/deploy.ps1 -o "%USERPROFILE%\.deploy.ps1" || echo "Unable to download deploy.ps1" && exit /B
 
-curl -Ssf https://raw.githubusercontent.com/senotrusov/stan-computer-deploy/master/deploy.ps1 -o .deploy.ps1 || echo "Unable to download deploy.ps1" && exit /B
-
-powershell -Command "Start-Process powershell \"-ExecutionPolicy Bypass -NoExit -Command %USERPROFILE%\.deploy.ps1\" -Wait -Verb RunAs"
+powershell -Command "Start-Process powershell \"-ExecutionPolicy Bypass -NoExit -Command `\"%USERPROFILE%\.deploy.ps1`\"\" -Wait -Verb RunAs"
