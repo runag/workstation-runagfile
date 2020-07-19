960f62ff02e733e0d94b5d95159266b9d3a4c846
Stanislav Senotrusov <stan@senotrusov.com>
2020-07-19 16:15:02 +0800

    -NoExit

--- deploy.bat
+++ deploy.bat
@@ -15,4 +15,4 @@ IF NOT EXIST C:\"Program Files"\Git\bin\sh.exe (
 
 curl -Ssf https://raw.githubusercontent.com/senotrusov/stan-computer-deploy/master/deploy.ps1 -o .deploy.ps1 || echo "Unable to download deploy.ps1" && exit /B
 
-powershell -Command "Start-Process powershell \"-ExecutionPolicy Bypass -Command %USERPROFILE%\.deploy.ps1\" -Wait -Verb RunAs"
+powershell -Command "Start-Process powershell \"-ExecutionPolicy Bypass -NoExit -Command %USERPROFILE%\.deploy.ps1\" -Wait -Verb RunAs"
