957fa0c8bcb633328ff3ed16ef2d09feeec47253
Stanislav Senotrusov <stan@senotrusov.com>
2020-08-14 13:03:32 +0000

    initial storage configuration

--- lib/ubuntu-storage-server.sh
+++ lib/ubuntu-storage-server.sh
@@ -36,25 +36,85 @@ ubuntu::deploy-storage-server() {
   # open-vm-tools
   if ubuntu::vmware::is-inside-vm; then
     apt::install open-vm-tools || fail
-    ubuntu::vmware::add-hgfs-automount || fail
-    ubuntu::vmware::symlink-hgfs-mounts || fail
+    # ubuntu::vmware::add-hgfs-automount || fail
+    # ubuntu::vmware::symlink-hgfs-mounts || fail
   fi
-
-  # avahi daemon
-  apt::install avahi-daemon || fail
 
   # ssh public key
   apt::install ssh-import-id || fail
   ssh-import-id gh:senotrusov || fail
 
+  # git
+  git::configure || fail
+
+  # avahi daemon
+  apt::install avahi-daemon || fail
+
   # cifs
   apt::install cifs-utils || fail
 
+  # rclone
+  ubuntu::install-rclone || fail
+
+  # borg
+  apt::install borgbackup || fail
+
   # cleanup
   apt::autoremove || fail
+
+  # storage configuration
+  (
+    bitwarden::unlock || fail
+    storage::configure-windows-share || fail
+    storage::configure-access-to-borg-storage || fail
+  ) || fail
 
   if [ -t 1 ]; then
     ubuntu::display-if-restart-required || fail
     tools::display-elapsed-time || fail
   fi
+}
+
+storage::configure-windows-share() {
+  local mountPoint="${HOME}/windows-documents"
+  local credentialsFile="${HOME}/.smbcredentials"
+  local fstabTag="# windows-documents cifs share"
+  local serverName="STAN-LAPTOP"
+  local bwItem="my microsoft account"
+  local cifsUsername
+  local cifsPassword
+
+  mkdir -p "${mountPoint}" || fail
+
+  if ! grep --quiet --fixed-strings --line-regexp "${fstabTag}" /etc/fstab; then
+    echo "${fstabTag}" | sudo tee --append /etc/fstab || fail
+    echo "//${serverName}/users/stan/Documents ${mountPoint} cifs credentials=${credentialsFile},file_mode=0640,dir_mode=0750,uid=${USER},gid=${USER} 0 0" | sudo tee --append /etc/fstab || fail
+  fi
+
+  if [ ! -f "${credentialsFile}" ]; then
+    bitwarden::unlock || fail
+    cifsUsername="$(bw get username "${bwItem}")" || fail
+    cifsPassword="$(bw get password "${bwItem}")" || fail
+    builtin printf "username=${cifsUsername}\npassword=${cifsPassword}\n" | (umask 077 && tee "${credentialsFile}" >/dev/null) || fail
+  fi
+
+  sudo mount -a || fail
+}
+
+storage::configure-access-to-borg-storage(){
+  local bwBorgStorageItem="my borg storage"
+
+  local borgStorageUsername
+  local borgStorageUri
+  local borgStorageHost
+  local borgStoragePort
+
+  borgStorageUsername="$(bw get username "${bwBorgStorageItem}")" || fail
+  borgStorageUri="$(bw get uri "${bwBorgStorageItem}")" || fail
+
+  borgStorageHost="$(echo "${borgStorageUri}" | cut -d ":" -f 1)" || fail
+  borgStoragePort="$(echo "${borgStorageUri}" | cut -d ":" -f 2)" || fail
+
+  ssh::install-keys "my borg storage ssh private key" "my borg storage ssh public key" || fail
+  ssh::add-host-to-known-hosts "${borgStorageHost}" "${borgStoragePort}" || fail
 }
