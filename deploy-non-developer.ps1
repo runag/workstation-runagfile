7eea1f38132f2e95725ad9be51696241a1845fe9
Stanislav Senotrusov <stan@senotrusov.com>
2020-09-12 09:07:57 +0800

    minor improvements

--- deploy-non-developer.ps1
+++ deploy-non-developer.ps1
@@ -20,17 +20,17 @@ if ($LASTEXITCODE -ne 0) {
   throw "Unable to upgrade installed packages"
 }
 
-choco install "$env:USERPROFILE\.bare-metal-desktop.config" --yes
+choco install "$env:USERPROFILE\.sopkafile\bare-metal-desktop.config" --yes
 if ($LASTEXITCODE -ne 0) {
   throw "Unable to install packages: bare-metal-desktop"
 }
 
-choco install "$env:USERPROFILE\.basic-utilities.config" --yes
+choco install "$env:USERPROFILE\.sopkafile\basic-utilities.config" --yes
 if ($LASTEXITCODE -ne 0) {
   throw "Unable to install packages: basic-utilities"
 }
 
-choco install "$env:USERPROFILE\.non-developer.config" --yes
+choco install "$env:USERPROFILE\.sopkafile\non-developer.config" --yes
 if ($LASTEXITCODE -ne 0) {
   throw "Unable to install packages: non-developer"
 }
@@ -39,7 +39,7 @@ $yes_no  = '&Yes', '&No'
 
 $choice = $Host.UI.PromptForChoice("Install packages: gamer?", "", $yes_no, 0)
 if ($choice -eq 0) {
-  choco install "$env:USERPROFILE\.gamer.config" --yes
+  choco install "$env:USERPROFILE\.sopkafile\gamer.config" --yes
   if ($LASTEXITCODE -ne 0) {
     throw "Unable to install packages: gamer"
   }
@@ -47,7 +47,7 @@ if ($choice -eq 0) {
 
 $choice = $Host.UI.PromptForChoice("Install packages: pdf-tools?", "", $yes_no, 0)
 if ($choice -eq 0) {
-  choco install "$env:USERPROFILE\.pdf-tools.config" --yes
+  choco install "$env:USERPROFILE\.sopkafile\pdf-tools.config" --yes
   if ($LASTEXITCODE -ne 0) {
     throw "Unable to install packages: pdf-tools"
   }
@@ -55,7 +55,7 @@ if ($choice -eq 0) {
 
 $choice = $Host.UI.PromptForChoice("Install packages: remote-control?", "", $yes_no, 0)
 if ($choice -eq 0) {
-  choco install "$env:USERPROFILE\.remote-control.config" --yes
+  choco install "$env:USERPROFILE\.sopkafile\remote-control.config" --yes
   if ($LASTEXITCODE -ne 0) {
     throw "Unable to install packages: remote-control"
   }
@@ -63,7 +63,7 @@ if ($choice -eq 0) {
 
 $choice = $Host.UI.PromptForChoice("Install packages: russian-teleconferencing?", "", $yes_no, 0)
 if ($choice -eq 0) {
-  choco install "$env:USERPROFILE\.russian-teleconferencing.config" --yes
+  choco install "$env:USERPROFILE\.sopkafile\russian-teleconferencing.config" --yes
   if ($LASTEXITCODE -ne 0) {
     throw "Unable to install packages: russian-teleconferencing"
   }
