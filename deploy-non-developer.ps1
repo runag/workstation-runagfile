1d10605d77d7b2312d2975fd45f6e73330193344
Stanislav Senotrusov <stan@senotrusov.com>
2020-09-12 17:03:29 +0800

    QOL improvements

--- deploy-non-developer.ps1
+++ deploy-non-developer.ps1
@@ -1,5 +1,12 @@
 $ErrorActionPreference = "Stop"
 
+$yes_no  = '&Yes', '&No'
+
+$choice_gamer = $Host.UI.PromptForChoice("Install packages: gamer?", "", $yes_no, 0)
+$choice_pdf_tools = $Host.UI.PromptForChoice("Install packages: pdf-tools?", "", $yes_no, 0)
+$choice_remote_control = $Host.UI.PromptForChoice("Install packages: remote-control?", "", $yes_no, 0)
+$choice_russian_teleconferencing = $Host.UI.PromptForChoice("Install packages: russian-teleconferencing?", "", $yes_no, 0)
+
 if (-Not (Get-Command "choco" -ErrorAction SilentlyContinue)) {
   Set-ExecutionPolicy Bypass -Scope Process -Force
   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
@@ -35,34 +42,28 @@ if ($LASTEXITCODE -ne 0) {
   throw "Unable to install packages: non-developer"
 }
 
-$yes_no  = '&Yes', '&No'
-
-$choice = $Host.UI.PromptForChoice("Install packages: gamer?", "", $yes_no, 0)
-if ($choice -eq 0) {
+if ($choice_gamer -eq 0) {
   choco install "$env:USERPROFILE\.sopkafile\gamer.config" --yes
   if ($LASTEXITCODE -ne 0) {
     throw "Unable to install packages: gamer"
   }
 }
 
-$choice = $Host.UI.PromptForChoice("Install packages: pdf-tools?", "", $yes_no, 0)
-if ($choice -eq 0) {
+if ($choice_pdf_tools -eq 0) {
   choco install "$env:USERPROFILE\.sopkafile\pdf-tools.config" --yes
   if ($LASTEXITCODE -ne 0) {
     throw "Unable to install packages: pdf-tools"
   }
 }
 
-$choice = $Host.UI.PromptForChoice("Install packages: remote-control?", "", $yes_no, 0)
-if ($choice -eq 0) {
+if ($choice_remote_control -eq 0) {
   choco install "$env:USERPROFILE\.sopkafile\remote-control.config" --yes
   if ($LASTEXITCODE -ne 0) {
     throw "Unable to install packages: remote-control"
   }
 }
 
-$choice = $Host.UI.PromptForChoice("Install packages: russian-teleconferencing?", "", $yes_no, 0)
-if ($choice -eq 0) {
+if ($choice_russian_teleconferencing -eq 0) {
   choco install "$env:USERPROFILE\.sopkafile\russian-teleconferencing.config" --yes
   if ($LASTEXITCODE -ne 0) {
     throw "Unable to install packages: russian-teleconferencing"
