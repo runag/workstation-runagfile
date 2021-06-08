0a31ed54aed2bf80cf82123d64d25f1e88a601f5
Stanislav Senotrusov <stan@senotrusov.com>
2021-06-08 08:46:50 +0200

    try lib/windows/packages/bare-metal-desktop.config in CI

--- deploy.ps1
+++ deploy.ps1
@@ -82,12 +82,12 @@ if ($LASTEXITCODE -ne 0) { throw "Unable to install restic" }
 
 
 # Install choco packages
-if ("$env:GITHUB_ACTIONS" -ne "true") { # gpg4win hangs forever in CI
+# if ("$env:GITHUB_ACTIONS" -ne "true") { # gpg4win hangs forever in CI
   if (-Not ((Get-WmiObject win32_computersystem).model -match "^VMware")) {
     choco install "$env:USERPROFILE\.sopkafile\lib\windows\packages\bare-metal-desktop.config" --yes
     if ($LASTEXITCODE -ne 0) { throw "Unable to install packages: bare-metal-desktop" }
   }
-}
+# }
 
 if ($install_developer_tools -eq 0) {
   choco install "$env:USERPROFILE\.sopkafile\lib\windows\packages\developer-tools.config" --yes
