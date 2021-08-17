aef1efe08769490b710468ceca1aafc8b76d6fc2
Stanislav Senotrusov <stan@senotrusov.com>
2021-08-17 20:08:57 +0200

    removes excessive copyright notes, they are artifacts of file merge

--- lib/ubuntu-workstation.sh
+++ lib/ubuntu-workstation.sh
@@ -253,22 +253,6 @@ esac
 SHELL
 }
 
-#!/usr/bin/env bash
-
-#  Copyright 2012-2021 Stanislav Senotrusov <stan@senotrusov.com>
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-
 ubuntu-workstation::install-shellrc() {
   shell::install-shellrc-directory-loader "${HOME}/.bashrc" || fail
   shell::install-sopka-path-shellrc || fail
@@ -457,22 +441,6 @@ ubuntu-workstation::lazy-install-secrets-dependencies() {
   fi
 }
 
-#!/usr/bin/env bash
-
-#  Copyright 2012-2021 Stanislav Senotrusov <stan@senotrusov.com>
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-
 ubuntu-workstation::configure-system() {
   # increase inotify limits
   linux::configure-inotify || fail
@@ -659,22 +627,6 @@ ubuntu-workstation::configure-gnome() {
     gnome-set desktop.peripherals.mouse speed -0.75 || fail
   ) || fail
 }
-
-#!/usr/bin/env bash
-
-#  Copyright 2012-2021 Stanislav Senotrusov <stan@senotrusov.com>
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 
 backup::vm-home-to-host() {
   backup::vm-home-to-host::load-configuration || fail
