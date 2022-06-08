93c96a467e9c3d17a3bd744f30a6528fff21ed09
Stanislav Senotrusov <stan@senotrusov.com>
2022-06-08 10:56:11 +0200

    tailscale

--- lib/ubuntu-workstation/deploy/tailscale.sh
+++ lib/ubuntu-workstation/deploy/tailscale.sh
@@ -21,27 +21,29 @@ ubuntu_workstation::deploy_tailscale() {
   # install bitwarden cli and login
   ubuntu_workstation::install_bitwarden_cli_and_login || fail
 
-  if [ "${SOPKA_UPDATE_SECRETS:-}" = true ] || ! command -v tailscale >/dev/null || tailscale::is_logged_out; then
+  # install tailscale
+  if ! command -v tailscale >/dev/null; then
+    bitwarden::beyond_session task::run_with_install_filter ubuntu_workstation::install_tailscale || fail
+  fi
+
+  if [ "${SOPKA_UPDATE_SECRETS:-}" = true ] || tailscale::is_logged_out; then
     bitwarden::unlock_and_sync || fail
 
     local tailscale_key; tailscale_key="$(bw get password "${MY_TAILSCALE_REUSABLE_KEY_ID}")" || fail
     
-    # shellcheck disable=2034
-    local SOPKA_TASK_STDERR_FILTER=task::install_filter
-    bitwarden::beyond_session task::run_with_short_title ubuntu_workstation::deploy_tailscale::stage_two "${tailscale_key}" || fail
+    bitwarden::beyond_session task::run_with_short_title ubuntu_workstation::tailscale_up "${tailscale_key}" || fail
   fi
 }
 
-ubuntu_workstation::deploy_tailscale::stage_two() {
-  local tailscale_key="$1"
-
-  # install tailscale
-  if ! command -v tailscale >/dev/null; then
-    tailscale::install || fail
-    if vmware::is_inside_vm; then
-      tailscale::install_issue_2541_workaround || fail
-    fi
+ubuntu_workstation::install_tailscale() {
+  tailscale::install || fail
+  if vmware::is_inside_vm; then
+    tailscale::install_issue_2541_workaround || fail
   fi
+}
+
+ubuntu_workstation::tailscale_up() {
+  local tailscale_key="$1"
 
   # logout if SOPKA_UPDATE_SECRETS is set
   if [ "${SOPKA_UPDATE_SECRETS:-}" = true ] && ! tailscale::is_logged_out; then
