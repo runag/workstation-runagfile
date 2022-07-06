c971ceaf6afd6e361d274d8b14435d4db3f28529
Stanislav Senotrusov <stan@senotrusov.com>
2022-07-06 09:54:17 +0200

    reorder

--- lib/ubuntu-workstation.sh
+++ lib/ubuntu-workstation.sh
@@ -14,6 +14,21 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+if [[ "${OSTYPE}" =~ ^linux ]] && declare -f sopka_menu::add >/dev/null; then
+  sopka_menu::add_header "Ubuntu workstation" || fail
+
+  sopka_menu::add ubuntu_workstation::dangerously_set_hostname || fail
+
+  if linux::display_if_restart_required::is_available; then
+    sopka_menu::add ubuntu_workstation::display_if_restart_required || fail
+  fi
+
+  if benchmark::is_available; then
+    sopka_menu::add ubuntu_workstation::run_benchmark || fail
+  fi
+
+  sopka_menu::add_delimiter || fail
+fi
 
 ubuntu_workstation::dangerously_set_hostname() {
   echo "Please keep in mind that the script to change hostname is not perfect, please take time to review the script and it's results"
@@ -31,19 +46,3 @@ ubuntu_workstation::display_if_restart_required() {
 ubuntu_workstation::run_benchmark() {
   benchmark::run || fail
 }
-
-if [[ "${OSTYPE}" =~ ^linux ]] && declare -f sopka_menu::add >/dev/null; then
-  sopka_menu::add_header "Ubuntu workstation" || fail
-
-  sopka_menu::add ubuntu_workstation::dangerously_set_hostname || fail
-
-  if linux::display_if_restart_required::is_available; then
-    sopka_menu::add ubuntu_workstation::display_if_restart_required || fail
-  fi
-
-  if benchmark::is_available; then
-    sopka_menu::add ubuntu_workstation::run_benchmark || fail
-  fi
-
-  sopka_menu::add_delimiter || fail
-fi
