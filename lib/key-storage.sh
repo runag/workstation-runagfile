db8ded6aa45bf354c0e62794404dd3c4c54e289b
Stanislav Senotrusov <stan@senotrusov.com>
2022-11-04 07:23:50 +0100

    better password store handling

--- lib/key-storage.sh
+++ lib/key-storage.sh
@@ -46,6 +46,23 @@ key_storage::add_sopka_menu_for_media() {
   
   sopka_menu::add key_storage::maintain_checksums "${media_path}" || fail
   sopka_menu::add key_storage::make_copies "${media_path}" || fail
+  
+  local password_store_remote_path="keys/password-store/workstation"
+  local password_store_path="${HOME}/.password-store"
+
+  if [ -d "${password_store_path}/.git" ]; then
+    if [ -d "${media_path}/${password_store_remote_path}" ]; then
+      sopka_menu::add key_storage::add_or_update_password_store_remote "${media_path}" "${password_store_remote_path}" || fail
+    else
+      sopka_menu::add key_storage::create_password_store_remote_repo "${media_path}" "${password_store_remote_path}" || fail
+    fi
+  fi
+  
+  if [ ! -d "${password_store_path}" ]; then
+    if [ -d "${media_path}/${password_store_remote_path}" ]; then
+      sopka_menu::add key_storage::clone_password_store_remote_repo_to_home "${media_path}" "${password_store_remote_path}" || fail
+    fi
+  fi
 }
 
 key_storage::maintain_checksums() {
@@ -79,6 +96,47 @@ key_storage::make_copies() {
 
   sync || fail
   echo "Copies were made: ${dest_dir}"
+}
+
+key_storage::add_or_update_password_store_remote() {(
+  local media_path="$1"
+  local password_store_remote_path="$2"
+
+  local repo_path="${media_path}/${password_store_remote_path}"
+  local remote_name; remote_name="$(basename "${media_path}")" || fail
+
+  local password_store_path="${HOME}/.password-store"
+
+  cd "${password_store_path}" || fail
+
+  git::add_or_update_remote "${remote_name}" "${repo_path}" || fail
+  git branch --move --force main || fail
+  git push --set-upstream "${remote_name}" main || fail
+)}
+
+key_storage::create_password_store_remote_repo() {
+  local media_path="$1"
+  local password_store_remote_path="$2"
+
+  local repo_path="${media_path}/${password_store_remote_path}"
+
+  git init --bare "${repo_path}" || fail
+
+  key_storage::add_or_update_password_store_remote "${media_path}" "${password_store_remote_path}" || fail
+}
+
+key_storage::clone_password_store_remote_repo_to_home() {
+  local media_path="$1"
+  local password_store_remote_path="$2"
+
+  local repo_path="${media_path}/${password_store_remote_path}"
+  local remote_name; remote_name="$(basename "${media_path}")" || fail
+
+  local password_store_path="${HOME}/.password-store"
+
+  if [ ! -d "${password_store_path}" ]; then
+    git clone --origin "${remote_name}" "${repo_path}" "${password_store_path}" || fail
+  fi
 }
 
 if declare -f sopka_menu::add >/dev/null; then
