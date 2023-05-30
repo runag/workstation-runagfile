d660431cd0522a1f6be2beb7861c7a2710d719b5
Stanislav Senotrusov <senotrusov@gmail.com>
2023-05-30 18:15:52 +0200

    --exit-status
    Signed-off-by: Stanislav Senotrusov <senotrusov@gmail.com>

--- lib/backup/commands.sh
+++ lib/backup/commands.sh
@@ -156,7 +156,7 @@ workstation::backup::restore() {
 
 workstation::backup::shell() {
   "${SHELL}"
-  softfail --code $? --unless-good "Abnormal termination of workstation::backup::shell ($?)"
+  softfail --exit-status $? --unless-good "Abnormal termination of workstation::backup::shell ($?)"
 }
 
 # shellcheck disable=2031
@@ -168,5 +168,5 @@ workstation::backup::remote_shell() {
   test "${remote_proto}" = sftp || softfail || return $?
 
   ssh -t "${remote_host}" "cd $(printf "%q" "${remote_path}"); exec \"\${SHELL}\" -l"
-  softfail --code $? --unless-good "Abnormal termination of workstation::backup::remote_shell ($?)"
+  softfail --exit-status $? --unless-good "Abnormal termination of workstation::backup::remote_shell ($?)"
 }
