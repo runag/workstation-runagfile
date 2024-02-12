6513b0a9d9374b2035b7651020906e95c1dba267
Stanislav Senotrusov <senotrusov@gmail.com>
2024-02-13 00:33:28 +0100

    apt::install
    Signed-off-by: Stanislav Senotrusov <senotrusov@gmail.com>

--- lib/linux/packages.sh
+++ lib/linux/packages.sh
@@ -186,16 +186,16 @@ workstation::linux::install_packages() {
   # software for bare metal workstation
   if linux::is_bare_metal; then
     # nvme-cli
-    sudo apt-get install nvme-cli || fail
+    apt::install install nvme-cli || fail
 
     # volume control
-    sudo apt-get install pavucontrol || fail
+    apt::install install pavucontrol || fail
 
     # webcam control
     apt::install v4l-utils || fail
 
     # vlc
-    sudo apt-get install vlc || fail
+    apt::install install vlc || fail
 
     # obs studio
     sudo add-apt-repository --yes ppa:obsproject/obs-studio || fail
